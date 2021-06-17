59ddc0acd2e723c158f593bde2150e8d5e574be5
buryk20 <73736907+buryk20@users.noreply.github.com>
2021-06-17 09:03:34 +0300

    sale

--- js/main.js
+++ js/main.js
@@ -19,28 +19,4 @@ window.onload = function () {
             }
         }
     });
-    // var informationOrder = document.querySelectorAll('.arrow-my-orders-icon');
-    // var ordersDropDownList = document.getElementsByClassName('orders-drop-down-list');
-
-    // informationOrder.forEach.call((el) => {
-    //     el.onclick = function (e) {
-    //         if (ordersDropDownList[0].hasAttribute("style")) {
-    //             // ordersDropDownList[0].style.display = 'flex';
-    //             ordersDropDownList[0].style.maxHeight = '300px';
-    //             ordersDropDownList[0].removeAttribute("style");
-    //             // ordersDropDownList[0].style.transition = 'all 4s ease-out 4s';
-    //             // document.getElementsByClassName('arrow-my-orders-icon')[0].style.transform = 'rotate(180deg)';
-    //         } else {
-    //             // ordersDropDownList[0].style.display = 'none';
-    //             ordersDropDownList[0].style.maxHeight = '300';
-    //             // document.getElementsByClassName('arrow-my-orders-icon')[0].style.transform = 'rotate(0deg)';
-    //         }
-    //     }
-
-
-
-
-    //     }
-    // });
-
 }
